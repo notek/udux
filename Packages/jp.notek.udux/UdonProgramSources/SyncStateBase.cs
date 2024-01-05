872dbb4a8e910a6496980f029bc51a35ed8d8d97
notoito <notoito.public+notek@gmail.com>
2024-01-05 23:10:12 +0900

    遅延時間計算の組み込み

--- Packages/jp.notek.udux/UdonProgramSources/SyncStateBase.cs
+++ Packages/jp.notek.udux/UdonProgramSources/SyncStateBase.cs
@@ -1,4 +1,5 @@
 using UdonSharp;
+using UnityEngine;
 using VRC.SDKBase;
 
 namespace JP.Notek.Udux
@@ -7,16 +8,32 @@ namespace JP.Notek.Udux
     public class SyncStateBase : UdonSharpBehaviour
     {
         public bool Initialized = false;
-        public bool IsOwner = false;
-        public VRCPlayerApi Owner = null;
+        public VRCPlayerApi Owner { get { return GetOwner(); } }
+        public bool IsOwner { get { return GetIsOwner(); } }
+        public float Latency = float.PositiveInfinity;
+
+        [UdonSynced] float _SyncTime;
+        [UdonSynced] float _SyncTimeId;
+        [SerializeField] int _LatencySyncFrameDuration = 1000;
+        float _SyncTimeIdLocal;
         IReduceStore _StoreSubscribed = null;
+        int _LatencySyncedFrame = 0;
+        void Start()
+        {
+            if (IsOwner)
+            {
+                SetLatency();
+                if (_StoreSubscribed != null)
+                    _StoreSubscribed.OnSyncStateDeserialization();
+            }
+        }
 
-
-        public void TakeOwnership()
+        public float TakeOwnership()
         {
             Networking.SetOwner(Networking.LocalPlayer, gameObject);
-            Owner = Networking.LocalPlayer;
-            IsOwner = true;
+            SetLatency();
+            RequestSerialization();
+            return Latency;
         }
 
         public void SubscribeOnDeserialization(IReduceStore store)
@@ -27,27 +44,42 @@ namespace JP.Notek.Udux
         public override void OnDeserialization()
         {
             Initialized = true;
+            if (_SyncTimeIdLocal != _SyncTimeId)
+            {
+                GetLatency();
+            }
             if (_StoreSubscribed != null)
                 _StoreSubscribed.OnSyncStateDeserialization();
         }
 
-        public override void OnOwnershipTransferred(VRCPlayerApi player)
-        {
-            Owner = player;
-            IsOwner = false;
-        }
-
         public override void OnPlayerJoined(VRCPlayerApi player)
         {
-            if (player.isLocal)
-            {
-                Owner = GetOwner();
-                IsOwner = GetIsOwner();
-            }
             if (IsOwner)
             {
                 RequestSerialization();
             }
+        }
+
+        new public void RequestSerialization()
+        {
+            if (Time.frameCount - _LatencySyncedFrame > _LatencySyncFrameDuration)
+                SetLatency();
+            base.RequestSerialization();
+        }
+
+        private void SetLatency()
+        {
+            _SyncTimeId = Random.value;
+            _SyncTimeIdLocal = _SyncTimeId;
+            _SyncTime = Time.time;
+            _LatencySyncedFrame = Time.frameCount;
+            Latency = 0;
+        }
+
+        private void GetLatency()
+        {
+            _SyncTimeIdLocal = _SyncTimeId;
+            Latency = Time.time - _SyncTime;
         }
 
         private VRCPlayerApi GetOwner()
